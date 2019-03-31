75858927c6c1cc93e5c762068321e3f20a522eec
Matt Urenovich <matt.urenovich@gmail.com>
2019-03-31 07:01:31 -0400

    Began Part 4: Started Adding Detail Views

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Matt Urenovich on 3/31/19.
+//  Copyright © 2019 Matt Urenovich. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
